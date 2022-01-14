1a4584d906d6784bcc7026a12e2970e988561c0c
Glen Robson <glenrobson@users.noreply.github.com>
2022-01-14 15:57:32 +0000

    Creating test release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.0.2.3'
+VERSION = '0.0.2.4'
 REQUIREMENTS = [
     "pydantic"
 ]
