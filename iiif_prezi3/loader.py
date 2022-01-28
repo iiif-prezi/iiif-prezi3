31453babbddb88f920b51d7b3d610f390375a8dc
Mike Bennett <mike.bennett@ed.ac.uk>
2022-01-28 14:31:23 +0000

    Rework extension loading

--- iiif_prezi3/loader.py
+++ iiif_prezi3/loader.py
@@ -2,20 +2,34 @@ import json
 import os
 
 
-def load_bundled_extensions(extension_file=None):
-    if not extension_file:
-        extension_file =  os.path.join(os.path.dirname(__file__), 'config', 'extensions.json')
-    try:
-        extensions = json.load(open(extension_file))
-    except FileNotFoundError:
-        return
+def load_bundled_extensions(extensions=None):
 
-    for ext in extensions:
-        __import__(f"iiif_prezi3.extensions.{ext}")
+    extensions_to_load = []
+
+    if extensions and type(extensions) == list:
+        extensions_to_load = extensions
+    elif not extensions or (extensions and type(extensions) == str):
+        if not extensions:
+            extension_file =  os.path.join(os.path.dirname(__file__), 'config', 'extensions.json')
+        else:
+            extension_file = extensions
+        try:
+            with open(extension_file, 'r') as extension_data:
+                extensions_to_load = json.load(extension_data)
+        except FileNotFoundError:
+            # TODO: Add a log error here
+            return
+
+    for ext in extensions_to_load:
+        load_extension(f"iiif_prezi3.extensions.{ext}")
 
 
 def load_extension(path):
-    pass
+    try:
+        __import__(path)
+    except ModuleNotFoundError:
+        # TODO: Add a log error here
+        return
 
 
 def monkeypatch_schema(schema_class, patch_classes):
