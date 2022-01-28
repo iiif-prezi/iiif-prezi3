90f09986be9c28f0456212c4f914b5c7c7496c52
Mike Bennett <mike.bennett@ed.ac.uk>
2022-01-28 14:31:45 +0000

    Add extension loading test

---
+++ tests/test_extensions.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.insert(1,'.')
+import json
+import unittest
+
+import iiif_prezi3
+
+# class NotCalledTest(unittest.TestCase):
+#     def test_no_call(self):
+#         self.assertNotIn('example_extension', dir(iiif_prezi3.extensions))
+
+class NoArgumentsTest(unittest.TestCase):
+    def test_no_arguments(self):
+        iiif_prezi3.load_bundled_extensions()
+        self.assertIn('example_extension', dir(iiif_prezi3.extensions))
+
+# class ListArgumentsTest(unittest.TestCase):
+#     def test_list_arguments(self):
+#         iiif_prezi3.load_bundled_extensions(['example_extension'])
+#         self.assertIn('example_extension', dir(iiif_prezi3.extensions))
+
+if __name__ == '__main__':
+    unittest.main()
