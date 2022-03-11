ce8f79d97a77b104721692488afcd6f2cbdff529
azaroth42 <azaroth42@gmail.com>
2022-03-11 11:59:30 -0500

    configuration

--- iiif_prezi3/base.py
+++ iiif_prezi3/base.py
@@ -5,12 +5,15 @@ class Base(BaseModel):
 
     def __init__(self, **kw):
         # Manipulate kw via defaulter helpers
-        if hasattr(self, 'defaulters'):
-            for df in self.defaulters:
+        if hasattr(self.__class__, '_defaulters'):
+            for df in self.__class__._defaulters:
                 update = df.generate_defaults(self, **kw)
                 if update:
                     kw.update(update)
+        else:
+            self.__class__._defaulters = []
         super().__init__(**kw)
+
 
     def __getattribute__(self, prop):
         val = super(Base, self).__getattribute__(prop)
