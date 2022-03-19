708d6a4560e6f14706cb807a795978ca8981ad22
Hillel Arnold <helrond@hotmail.com>
2022-03-19 15:59:22 -0400

    rename test file

--- tests/test_canvas_helpers.py
+++ tests/test_canvas_helpers.py
@@ -11,5 +11,7 @@ class CanvasHelpersTests(unittest.TestCase):
         self.assertTrue(isinstance(anno_page, AnnotationPage), '`add_image` should return an AnnotationPage')
         self.assertEqual(len(self.canvas.items), 1)
         self.assertEqual(anno_page.items[0].id, 'http://iiif.example.org/prezi/Annotation/0')
-        # this seems wrong, shouldn't it be the id property?
-        self.assertTrue(anno_page.items[0].body, 'http://iiif.example.org/prezi/Image/0')
+        self.assertEqual(anno_page.items[0].target, 'http://iiif.example.org/prezi/Image/0')
+        print(anno_page.items[0].body)
+        # this test is failing because anno_page.items[0].body has the type of `str`
+        self.assertEqual(anno_page.items[0].body.id, 'http://iiif.example.org/prezi/Image/0')
