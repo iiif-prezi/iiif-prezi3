31a5edfa113350bc37ea494301adb8f02dcb0da5
Hillel Arnold <helrond@hotmail.com>
2022-04-27 13:43:28 -0400

    add docstring

--- iiif_prezi3/helpers/canvas_helpers.py
+++ iiif_prezi3/helpers/canvas_helpers.py
@@ -4,6 +4,19 @@ from ..loader import monkeypatch_schema
 
 
 class AddImageToCanvas:
+    """Adds an image to an existing canvas.
+
+    Args:
+        image_url (str): An HTTP URL which points to the image.
+        anno_id (str): An HTTP URL for the annotation to which the image will be
+            attached.
+        anno_page_id (str): An HTTP URL for the annotation page to which the
+            annotation will be attached.
+
+    Returns:
+        anno_page (iiif-prezi3.skeleton.AnnotationPage): the AnnotationPage with
+            an Annotation and ResourceItem attached.
+    """
     def add_image(self, image_url, anno_id=None, anno_page_id=None):
         body = ResourceItem(id=image_url, type='Image')
         annotation = Annotation(id=anno_id, body=body, target=image_url, motivation='painting', type='Annotation')
