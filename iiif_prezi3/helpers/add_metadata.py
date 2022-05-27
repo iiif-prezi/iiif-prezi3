0142fcbfc53f604fa243554b7cb655604b5edf69
Duncan Saunders <duncan.saunders@digirati.com>
2022-05-27 15:13:10 +0100

    first pass at add_metadata

---
+++ iiif_prezi3/helpers/add_metadata.py
@@ -0,0 +1,12 @@
+from ..skeleton import Collection, Manifest, Canvas, Range
+from ..loader import monkeypatch_schema
+from iiif_prezi3 import KeyValueString
+
+class AddMetadata:
+    def add_metadata(self, label, value):
+        if not self.metadata:
+            self.metadata = []
+        kv = KeyValueString(label="label", value="value")
+        self.metadata.append(kv)
+
+monkeypatch_schema([Collection, Manifest, Canvas, Range], AddMetadata)
