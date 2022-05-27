f7664e6a66c409f03da5fc649c26d28b4cf72a81
Etienne Posthumus <eposthumus@gmail.com>
2022-05-27 17:33:34 +0200

    Make it Black

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 # read the contents of your README file
 from pathlib import Path
 
@@ -12,38 +13,36 @@ if os.path.exists("version.txt"):
 else:
     VERSION = "local_test_version"
 
-REQUIREMENTS = [
-    "pydantic"
-]
+REQUIREMENTS = ["pydantic"]
 
 # Setting up
 setup(
-    name='iiif-prezi3',
+    name="iiif-prezi3",
     version=VERSION,
-    author='IIIF Prezi3 Team',
-    description='IIIF Presentation v3 API implementation',
+    author="IIIF Prezi3 Team",
+    description="IIIF Presentation v3 API implementation",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=['iiif_prezi3', 'iiif_prezi3.helpers', 'iiif_prezi3.extensions'],
-    package_data={
-        'iiif_prezi3': ['config/extensions.json']
-    },
-    classifiers=["Development Status :: 4 - Beta",
-                 "Intended Audience :: Developers",
-                 "License :: OSI Approved :: Apache Software License",
-                 "Operating System :: OS Independent",  # is this true? know Linux & OS X ok
-                 "Programming Language :: Python",
-                 "Programming Language :: Python :: 3",
-                 "Programming Language :: Python :: 3.7",
-                 "Programming Language :: Python :: 3.8",
-                 "Programming Language :: Python :: 3.9",
-                 "Programming Language :: Python :: 3.10",
-                 "Topic :: Internet :: WWW/HTTP",
-                 "Topic :: Multimedia :: Graphics :: Graphics Conversion",
-                 "Topic :: Software Development :: Libraries :: Python Modules",
-                 "Environment :: Web Environment"],
-    python_requires='>=3',
-    url='https://github.com/iiif-prezi/iiif-prezi3',
-    license='LICENSE',
+    long_description_content_type="text/markdown",
+    packages=["iiif_prezi3", "iiif_prezi3.helpers", "iiif_prezi3.extensions"],
+    package_data={"iiif_prezi3": ["config/extensions.json"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",  # is this true? know Linux & OS X ok
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+    ],
+    python_requires=">=3",
+    url="https://github.com/iiif-prezi/iiif-prezi3",
+    license="LICENSE",
     install_requires=REQUIREMENTS,
 )
