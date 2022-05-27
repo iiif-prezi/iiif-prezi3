43db51a152b227f3c7c769a98074f9fb99628e4c
Mike Bennett <mike.bennett@ed.ac.uk>
2022-05-27 14:36:47 +0100

    Update monkeypath to handle list of Schema classes

--- iiif_prezi3/loader.py
+++ iiif_prezi3/loader.py
@@ -32,11 +32,21 @@ def load_extension(path):
         return
 
 
-def monkeypatch_schema(schema_class, patch_classes):
-    schema_bases = list(schema_class.__bases__)
-    if type(patch_classes) == list:
-        for c in patch_classes:
-            schema_bases.append(c)
+def monkeypatch_schema(schema_classes, patch_classes):
+    if type(schema_classes) == list:
+        for schema_class in schema_classes:
+            schema_bases = list(schema_class.__bases__)
+            if type(patch_classes) == list:
+                for c in patch_classes:
+                    schema_bases.append(c)
+            else:
+                schema_bases.append(patch_classes)
+            schema_class.__bases__ = tuple(schema_bases)
     else:
-        schema_bases.append(patch_classes)
-    schema_class.__bases__ = tuple(schema_bases)
+        schema_bases = list(schema_classes.__bases__)
+        if type(patch_classes) == list:
+            for c in patch_classes:
+                schema_bases.append(c)
+        else:
+            schema_bases.append(patch_classes)
+        schema_classes.__bases__ = tuple(schema_bases)
