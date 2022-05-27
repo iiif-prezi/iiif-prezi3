1b84b0d3d0673518863e9c4b169e1aa84916a266
giacomounivr <giacomo.marchioro@univr.it>
2022-05-27 19:28:40 +0200

    Raise error for duration not accepting floats

--- tests/test_set_hwd.py
+++ tests/test_set_hwd.py
@@ -10,42 +10,42 @@ sys.path.insert(1, '.')
 
 class Set_hwd(unittest.TestCase):
 
-    def test_assignment(self):
-        """Test if the assignment of height, width, and duration works."""
-        height = '100'
-        width = '200'
-        duration = '300.23'
+    def setUp(self):
         canvasid = 'http://iiif.example.org/prezi/CanvasID/01'
         acanvas = Canvas(id=canvasid, type='Canvas', label={'en': ['default lab']})
-        acanvas.set_hwd(height=height, width=width, duration=duration)
-        self.assertEqual(height, str(acanvas.height))
-        self.assertEqual(width, str(acanvas.width))
-        self.assertEqual(duration, str(acanvas.duration))
+        self.acanvas = acanvas
+
+    def test_assignment(self):
+        """Test if the assignment of height, width, and duration works."""
+        height = 100
+        width = 200
+        #TODO: duration is not yet supported as float I pass it as string
+        duration = '300.23'
+        self.acanvas.set_hwd(height=height, width=width, duration=duration)
+        self.assertEqual(height, self.acanvas.height)
+        self.assertEqual(width, self.acanvas.width)
+        self.assertEqual(duration, self.acanvas.duration)
 
     def test_arguments_combination(self):
         """If a Canvas has either of height and width, it must have the other."""
         height = '100'
         width = '200'
         duration = '300.23'
-        canvasid = 'http://iiif.example.org/prezi/CanvasID/01'
-        acanvas = Canvas(id=canvasid, type='Canvas', label={'en': ['default lab']})
         with self.assertRaises(TypeError):
-            acanvas.set_hwd(height=height, width=None, duration=duration)
+            self.acanvas.set_hwd(height=height, width=None, duration=duration)
         with self.assertRaises(TypeError):
-            acanvas.set_hwd(height=None, width=width, duration=duration)
+            self.acanvas.set_hwd(height=None, width=width, duration=duration)
         with self.assertRaises(TypeError):
-            acanvas.set_hwd(height=None, width=None, duration=None)
+            self.acanvas.set_hwd(height=None, width=None, duration=None)
 
     def test_pydantic_validation(self):
         """Test if pydantic validate the assignment of height, width, and duration."""
         height = 'test'
         width = 'test'
         duration = 'test'
-        canvasid = 'http://iiif.example.org/prezi/CanvasID/01'
-        acanvas = Canvas(id=canvasid, type='Canvas', label={'en': ['default lab']})
         with self.assertRaises(ValidationError):
-            acanvas.set_hwd(height=height, width=134, duration=None)
+            self.acanvas.set_hwd(height=height, width=134, duration=None)
         with self.assertRaises(ValidationError):
-            acanvas.set_hwd(height=123, width=width, duration=None)
+            self.acanvas.set_hwd(height=123, width=width, duration=None)
         with self.assertRaises(ValidationError):
-            acanvas.set_hwd(height=None, width=None, duration=duration)
+            self.acanvas.set_hwd(height=None, width=None, duration=duration)
