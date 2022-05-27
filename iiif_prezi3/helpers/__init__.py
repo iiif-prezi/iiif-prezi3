9004444d4238fd3a5c6fdd7bbd8dd39c2366ceb7
Robert Casties <casties@mpiwg-berlin.mpg.de>
2022-05-27 17:28:53 +0200

    AnnotationHelpers to add Annotations to Collection, Manifest, etc.

--- iiif_prezi3/helpers/__init__.py
+++ iiif_prezi3/helpers/__init__.py
@@ -2,3 +2,4 @@ from .auto_fields import *  # noqa: F401,F403
 from .canvas_helpers import AddImageToCanvas  # noqa: F401
 from .canvas_sizes import MaxHelper, MinHelper  # noqa: F401
 from .collection_add_methods import AddToCollection  # noqa: F401
+from .annotation_helpers import AnnotationHelpers
---
+++ tests/test_annotation_helpers.py
@@ -0,0 +1,22 @@
+import unittest
+
+from iiif_prezi3 import Manifest, Annotation, AnnotationPage
+
+
+class AnnotationHelpersTests(unittest.TestCase):
+    def setUp(self):
+        self.Manifest = Manifest(id='http://iiif.example.org/prezi/Manifest/0', label={'en': ['default label']})
+
+    def test_add_annotation(self):
+        anno_target = 'http://iiif.example.org/prezi/Annotation/0'
+        anno = Annotation(target=anno_target)
+        m = Manifest(id='http://iiif.example.org/prezi/Manifest/0', label={'en': ['Test Manifest']})
+        m.add_annotation(anno)
+        # test
+        self.assertEqual(len(m.annotations), 1)
+        anno_page = m.annotations[0]
+        self.assertTrue(isinstance(anno_page, AnnotationPage), '`annotations` should contain an AnnotationPage')
+        self.assertEqual(len(anno_page.items), 1)
+        #self.assertEqual(anno_page.items[0].id, 'http://iiif.example.org/prezi/Annotation/0')
+        self.assertEqual(anno_page.items[0].target, anno_target)
+        #self.assertEqual(anno_page.items[0].dict()["body"]["id"], 'http://iiif.example.org/prezi/Image/0')
