ef0a8d4f33194e64a0a65bab5beb38c5cb0e3f9c
Duncan Saunders <duncan.saunders@digirati.com>
2022-05-30 09:41:39 +0100

    Accepts dict & LngString params. Validation & tests.

--- tests/test_add_metadata.py
+++ tests/test_add_metadata.py
@@ -1,14 +1,40 @@
 import unittest
 
-from iiif_prezi3 import Canvas, Collection, LngString, Manifest, Range
+from iiif_prezi3 import Collection, Manifest, Canvas, Range, LngString
 
 
 class AddMetadataTest(unittest.TestCase):
     def setUp(self):
-        self.collection = Collection()
+        self.collection = Collection();
 
-    def test_add_metadata(self):
-        self.collection.add_metadata("label", "value")
+    def test_add_metadata_string(self):
+        self.collection.add_metadata("label", "value");
         assert isinstance(self.collection.metadata, list)
         assert self.collection.metadata[-1].label == {'none': ['label']}
         assert self.collection.metadata[-1].value == {'none': ['value']}
+
+    def test_add_metadata_dict(self):
+        label = {"en": ["label"]}
+        value = {"en": ["value"]}
+        self.collection.add_metadata(label, value);
+        assert isinstance(self.collection.metadata, list)
+        assert self.collection.metadata[-1].label == {'en': ['label']}
+        assert self.collection.metadata[-1].value == {'en': ['value']}
+
+    def test_add_metadata_lngstring(self):
+        label = LngString(__root__={'en': ['label']})
+        value = LngString(__root__={'en': ['value']})
+        self.collection.add_metadata(label, value);
+        assert isinstance(self.collection.metadata, list)
+        assert self.collection.metadata[-1].label == {'en': ['label']}
+        assert self.collection.metadata[-1].value == {'en': ['value']}
+
+    def test_validate_different_argtypes(self):
+        label = "label"
+        value = 5
+        self.assertRaises(TypeError, self.collection.add_metadata, label, value)
+
+    def test_invalid_argtypes(self):
+        label = ["label"]
+        value = ["value"]
+        self.assertRaises(TypeError, self.collection.add_metadata, label, value)
