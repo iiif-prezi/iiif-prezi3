d38f595a2efde9d8022c7abfc4136d47bbcb0217
Mike Bennett <mbennett@datacite.org>
2022-10-21 19:31:50 +0100

    Add make_range to helpers init

--- iiif_prezi3/helpers/__init__.py
+++ iiif_prezi3/helpers/__init__.py
@@ -8,6 +8,7 @@ from .canvas_helpers import AddImageToCanvas  # noqa: F401
 from .canvas_sizes import MaxHelper, MinHelper  # noqa: F401
 from .make_collection import MakeCollection  # noqa: F401
 from .make_manifest import MakeManifest  # noqa: F401
+from .make_range import MakeRange  # noqa: F401
 from .set_hwd import SetHwd  # noqa: F401
 from .set_hwd_from_file import SetHeightWidthDurationFileHelper  # noqa: F401
 from .set_hwd_from_iiif import SetHwdFromIIIF  # noqa: F401
