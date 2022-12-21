024300503bfb7b4ed31e427d232bd8856da1e19b
Mike Bennett <mbennett@datacite.org>
2022-12-21 18:58:00 +0000

    Add explicit imports for auto_fields helpers

--- iiif_prezi3/helpers/__init__.py
+++ iiif_prezi3/helpers/__init__.py
@@ -4,7 +4,7 @@ from .add_metadata import AddMetadata  # noqa: F401
 from .add_range import AddRange  # noqa: F401
 from .add_service import AddService  # noqa: F401
 from .annotation_helpers import AnnotationHelpers  # noqa: F401
-from .auto_fields import *  # noqa: F401,F403
+from .auto_fields import ai, al, ait  # noqa: F401,F403
 from .canvas_helpers import AddImageToCanvas  # noqa: F401
 from .canvas_sizes import MaxHelper, MinHelper  # noqa: F401
 from .create_canvas_from_iiif import CreateCanvasFromIIIF  # noqa: F401
