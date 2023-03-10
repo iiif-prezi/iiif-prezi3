baf79f8e1947c4e9d9d2087af8eb6d5e3f6533c3
Mike Bennett <mbennett@datacite.org>
2023-03-10 13:35:21 +0000

    Move add_thumbnail to separate helper

---
+++ iiif_prezi3/helpers/add_thumbnail.py
@@ -0,0 +1,22 @@
+from ..loader import monkeypatch_schema
+from ..skeleton import Canvas, ResourceItem, AccompanyingCanvas, PlaceholderCanvas
+
+
+class AddThumbnail:
+    def add_thumbnail(self, image_url, **kwargs):
+        """Adds a thumbnail to an existing canvas.
+
+        Args:
+            image_url (str): An HTTP URL which points to the thumbnail.
+            **kwargs (): see ResourceItem.
+
+        Returns:
+            new_thumbnail (ResourceItem): the newly-created thumbnail.
+        """
+        new_thumbnail = ResourceItem(id=image_url, type='Image', **kwargs)
+        if not self.thumbnail:
+            self.thumbnail = list()
+        self.thumbnail.append(new_thumbnail)
+        return new_thumbnail
+
+monkeypatch_schema([Canvas, PlaceholderCanvas, AccompanyingCanvas], AddThumbnail)
