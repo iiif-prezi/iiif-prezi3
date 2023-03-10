85c79653bba86032651d78213fd5167f56d3f3c4
giacomounivr <giacomo.marchioro@univr.it>
2023-03-10 10:19:50 +0100

    Corrects recipe

--- docs/recipes/scripts/0118-multivalue-method1.py
+++ docs/recipes/scripts/0118-multivalue-method1.py
@@ -7,12 +7,15 @@ manifest.metadata = [
                    value={"en": ["Whistler's Mother", "Arrangement in Grey and Black No. 1"],
                           "fr": ["Portrait de la mère de l'artiste", "La Mère de Whistler"]})
 ]
-manifest.summary = {"en": ["A painting in oil on canvas created by the American-born painter James McNeill Whistler, in 1871."],
-                    "fr": ["Arrangement en gris et noir n°1, also called Portrait de la mère de l'artiste."]}
+manifest.summary = {"en": ["A painting in oil on canvas created by the American-born painter James McNeill Whistler, in 1871."]}
 
-canvas = manifest.make_canvas_from_iiif(url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Whistlers_Mother_high_res.jpg/1114px-Whistlers_Mother_high_res.jpg",
-                                        id="https://iiif.io/api/cookbook/recipe/0118_multivalue/canvas/1")
-canvas.items[0].id = "https://iiif.io/api/cookbook/recipe/0118_multivalue/canvas/1/page/1"
+canvas = manifest.make_canvas(id="https://iiif.io/api/cookbook/recipe/0118_multivalue/canvas/1", height=991, width=1114)
+anno_page = canvas.add_image(image_url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Whistlers_Mother_high_res.jpg/1114px-Whistlers_Mother_high_res.jpg",
+                             anno_page_id="https://iiif.io/api/cookbook/recipe/0118_multivalue/canvas/1/page/1",
+                             anno_id="https://iiif.io/api/cookbook/recipe/0118_multivalue/canvas/1/page/1/annotation/1",
+                             format="image/jpeg",
+                             )
+
 canvas.items[0].items[0].id = "https://iiif.io/api/cookbook/recipe/0118_multivalue/canvas/1/page/1/annotation/1"
 
-print(manifest.json(indent=2))
+print(manifest.json(indent=2, ensure_ascii=False))
