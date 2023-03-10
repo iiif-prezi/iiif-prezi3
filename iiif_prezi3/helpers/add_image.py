902dcdefd368fd5f4980e350443a6ff48f078f1c
Mike Bennett <mbennett@datacite.org>
2023-03-10 13:33:21 +0000

    Move add_image to separate helper

---
+++ iiif_prezi3/helpers/add_image.py
@@ -0,0 +1,27 @@
+from ..loader import monkeypatch_schema
+from ..skeleton import Annotation, AnnotationPage, Canvas, ResourceItem, AccompanyingCanvas, PlaceholderCanvas
+
+
+class AddImage:
+
+    def add_image(self, image_url, anno_id=None, anno_page_id=None, **kwargs):
+        """Adds an image to an existing canvas.
+
+        Args:
+            image_url (str): An HTTP URL which points to the image.
+            anno_id (str): An HTTP URL for the annotation to which the image will be attached.
+            anno_page_id (str): An HTTP URL for the annotation page to which the annotation will be attached.
+
+        Returns:
+            anno_page (AnnotationPage): the AnnotationPage with an Annotation and ResourceItem attached.
+        """
+        body = ResourceItem(id=image_url, type='Image', **kwargs)
+        annotation = Annotation(id=anno_id, body=body, target=self.id, motivation='painting', type='Annotation')
+        anno_page = AnnotationPage(id=anno_page_id, type='AnnotationPage', items=[annotation])
+        if not self.items:
+            self.items = list()
+        self.items.append(anno_page)
+        return anno_page
+
+
+monkeypatch_schema([Canvas, AccompanyingCanvas, PlaceholderCanvas], AddImage)
