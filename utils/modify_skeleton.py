92c3a5cd022577d39ed48c1f7ae6a2cb554d688d
Mike Bennett <mbennett@datacite.org>
2023-06-01 17:24:36 +0200

    Update skeleton modifier data to be accurate for new changes

--- utils/modify_skeleton.py
+++ utils/modify_skeleton.py
@@ -2,7 +2,7 @@ CHANGES = [
     {
         "description": "Allow extra properties on ManifestRef",
         "type": "insert",
-        "before": "class ManifestRef(Reference):\n    type: Optional[constr(regex=r'^Manifest$')] = None\n",
+        "before": "class ManifestRef(Reference):\n    type: constr(regex=r'^Manifest$') = 'Manifest'\n",
         "after": "\n\nclass CanvasRef(Reference):",
         "data": "\n    class Config:\n        extra = Extra.allow\n"
     },
@@ -10,7 +10,7 @@ CHANGES = [
         "description": "Re-add RangeRef",
         "type": "insert",
         "before": "class CanvasRef(Reference):\n    type: Optional[constr(regex=r'^Canvas$')] = None\n",
-        "after": "\n\n",
+        "after": "\n\nModel.update_forward_refs()",
         "data": "\n\nclass RangeRef(Reference):\n    type: Optional[constr(regex=r'^Range$')] = None\n"
     }
 ]
