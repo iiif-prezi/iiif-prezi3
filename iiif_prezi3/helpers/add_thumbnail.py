236ac54642ef1e96b0542ac7288677f63ca7f60d
Mark Baggett <markpbaggett@gmail.com>
2024-12-23 12:14:59 -0600

    Update messy code with passing tests before refactor.

--- iiif_prezi3/helpers/add_thumbnail.py
+++ iiif_prezi3/helpers/add_thumbnail.py
@@ -23,7 +23,10 @@ class AddThumbnail:
         return new_thumbnail
 
     def create_thumbnail_from_iiif(self, url, preferred_width=500, **kwargs):
-        """Adds an image thumbnail to a manifest or canvas based on a IIIF service.
+        """Adds an image thumbnail to a manifest or canvas based on a IIIF service. If there is a sizes property, it
+        returns the thumbnail that is closest to the preferred width but larger. If no sizes property exists and it's
+        not level zero image service, it returns a thumbnail with the exact preferred width. Else, it returns the level
+        `full/full` or `full/max`.
 
         Args:
             url (str): An HTTP URL which points at a IIIF Image response.
@@ -36,15 +39,17 @@ class AddThumbnail:
         image_response = ResourceItem(id=url, type='Image')
         image_info = image_response.set_hwd_from_iiif(url)
         context = image_info.get('@context', '')
+        aspect_ratio = image_info.get('width') / image_info.get('height')
 
         if 'sizes' in image_info:
-            best_fit_size = max(
-                (size for size in image_info['sizes'] if size["width"] <= preferred_width),
+            best_fit_size = min(
+                (size for size in image_info['sizes'] if size["width"] >= preferred_width),
                 key=lambda size: size["width"],
-                default=image_info['sizes'][0]
+                default=image_info['sizes'][-1]
             )
             thumbnail_id = f"{url.replace('/info.json', '')}/full/{best_fit_size['width']},{best_fit_size['height']}/0/default.jpg"
         else:
+            # Set a default thumbnail in case of level 0 with no size property
             thumbnail_id = f"{url.replace('/info.json', '')}/full/full/0/default.jpg" if context == "http://iiif.io/api/image/2/context.json" else f"{url.replace('/info.json', '')}/full/max/0/default.jpg"
 
         if context == "http://iiif.io/api/image/2/context.json":
@@ -63,13 +68,18 @@ class AddThumbnail:
             )
             if profile == "http://iiif.io/api/image/2/level0.json" and 'sizes' in image_info:
                 thumbnail_id = f"{url.replace('/info.json', '')}/full/{best_fit_size['width']},/0/default.jpg"
+            elif profile != "http://iiif.io/api/image/2/level0.json" and 'sizes' not in image_info:
+                thumbnail_id = f"{url.replace('/info.json', '')}/full/{preferred_width},/0/default.jpg"
         else:
+            profile = image_info.get('profile', '')
             service = ServiceItem(
                 id=image_info['id'],
-                profile=image_info.get('profile', ''),
+                profile=profile,
                 type=image_info.get('type', 'ImageService'),
                 format="image/jpeg"
             )
+            if profile != "level0" and 'sizes' not in image_info:
+                thumbnail_id = f"{url.replace('/info.json', '')}/full/{preferred_width},{int(preferred_width/aspect_ratio)}/0/default.jpg"
 
         new_thumbnail = ResourceItem(
             id=thumbnail_id,
@@ -80,7 +90,10 @@ class AddThumbnail:
         if 'sizes' in image_info:
             new_thumbnail.height = best_fit_size.get('height')
             new_thumbnail.width = best_fit_size.get('width')
-        elif 'height' in image_info and 'width' in image_info:
+        elif profile != "http://iiif.io/api/image/2/level0.json" or 'profile' != "level0":
+            new_thumbnail.width = preferred_width
+            new_thumbnail.height = preferred_width/aspect_ratio
+        else:
             new_thumbnail.height = image_info['height']
             new_thumbnail.width = image_info['width']
 
@@ -93,4 +106,8 @@ class AddThumbnail:
         return self.thumbnail
 
 
-monkeypatch_schema([Canvas, PlaceholderCanvas, AccompanyingCanvas, AnnotationPage, Collection, Manifest, Annotation, Range, ResourceItem, AnnotationCollection, Reference], AddThumbnail)
+monkeypatch_schema(
+    [Canvas, PlaceholderCanvas, AccompanyingCanvas, AnnotationPage, Collection, Manifest, Annotation,
+     Range, ResourceItem, AnnotationCollection, Reference],
+    AddThumbnail
+)
