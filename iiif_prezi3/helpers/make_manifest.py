66c1b94e42129d290166d28406816ac1c4a8ee7c
Mark Baggett <markpbaggett@gmail.com>
2025-01-09 14:06:50 -0600

    Attempt to address Issue-191.

--- iiif_prezi3/helpers/make_manifest.py
+++ iiif_prezi3/helpers/make_manifest.py
@@ -1,19 +1,19 @@
 from ..loader import monkeypatch_schema
-from ..skeleton import Collection, Manifest
+from ..skeleton import Collection, ManifestRef
 
 
 class MakeManifest:
 
     def make_manifest(self, **kwargs):
-        """Add a Manifest to a Collection.
+        """Add a Manifest to a Collection as a ManifestRef.
 
         Creates a new Manifest, adds a Reference to it to the
         calling Collection items and returns the newly created Manifest.
         Accepts keyword arguments to customize the resulting instance.
         """
-        manifest = Manifest(**kwargs)
+        manifest = ManifestRef(**kwargs)
         self.add_item(manifest)
-        return manifest
 
+        return manifest
 
 monkeypatch_schema(Collection, MakeManifest)
--- tests/test_make_manifest.py
+++ tests/test_make_manifest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from iiif_prezi3 import Collection, Reference
+from iiif_prezi3 import Collection, Reference, HomepageItem, config
 
 
 class MakeManifestTest(unittest.TestCase):
@@ -9,9 +9,21 @@ class MakeManifestTest(unittest.TestCase):
         self.collection = Collection()
 
     def test_make_manifest(self):
+        config.configs['helpers.auto_fields.AutoLang'].auto_lang = "en"
+        homepage = HomepageItem(
+            id="https://www.getty.edu/art/collection/object/103RQQ",
+            type="Text",
+            label="Home page at the Getty Museum Collection",
+            format="text/html",
+            language=["en"]
+        )
         manifest = self.collection.make_manifest(
-            label={'en': ['default label']})
+            id='http://example.org/iiif/1',
+            label='default label',
+            homepage=homepage,
+        )
         self.assertEqual(len(self.collection.items), 1)
         self.assertIsInstance(self.collection.items[0], Reference)
         self.assertEqual(manifest.label,
                          {'en': ['default label']})
+        self.assertEqual(manifest.homepage[0], homepage)
