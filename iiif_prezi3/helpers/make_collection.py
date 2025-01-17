4ec242891bda9196abe0bd103f261e2a937b7a5d
Mark Baggett <markpbaggett@gmail.com>
2025-01-16 18:16:15 -0600

    Init fix for Issue 225.

--- iiif_prezi3/helpers/make_collection.py
+++ iiif_prezi3/helpers/make_collection.py
@@ -1,17 +1,22 @@
 from ..loader import monkeypatch_schema
-from ..skeleton import Collection
+from ..skeleton import Collection, CollectionRef
 
 
 class MakeCollection:
 
     def make_collection(self, **kwargs):
-        """Create a Collection.
+        """Creates a Collection or CollectionRef.
 
         Creates a new collection, adds it to the calling Collection `items`
         and returns the newly created object. Accepts keyword arguments to
         customize the resulting instance.
         """
-        child_collection = Collection(**kwargs)
+        if 'items' not in kwargs:
+            child_collection = CollectionRef(**kwargs)
+        elif len(kwargs['items']) == 0:
+            child_collection = CollectionRef(**kwargs)
+        else:
+            child_collection = Collection(**kwargs)
         self.add_item(child_collection)
         return child_collection
 
