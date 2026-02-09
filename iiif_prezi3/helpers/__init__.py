d75d7e5c3813472ebf9e2d1ffa7b933810b8ed4a
Mark Baggett <markpbaggett@gmail.com>
2026-02-08 23:41:51 -0600

    Define navPlace helpers.

--- iiif_prezi3/helpers/__init__.py
+++ iiif_prezi3/helpers/__init__.py
@@ -11,6 +11,7 @@ from .auto_fields import *  # noqa: F401,F403
 from .canvas_sizes import MaxHelper, MinHelper  # noqa: F401
 from .create_canvas_from_iiif import CreateCanvasFromIIIF  # noqa: F401
 from .make_canvas import MakeCanvas  # noqa: F401
+from .make_navplace_feature import MakeNavPlaceFeature, AddNavPlaceFeature  # noqa: F401
 from .make_collection import MakeCollection  # noqa: F401
 from .make_manifest import MakeManifest  # noqa: F401
 from .make_range import MakeRange  # noqa: F401
