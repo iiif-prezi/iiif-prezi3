56379c52f5311a96b0ff4c5c9b4f7023566e6be9
Glen Robson <glen.robson@gmail.com>
2026-09-23 13:45:11 +0100

    Label also changed

--- docs/recipes/scripts/0001-mvm-image-method2.py
+++ docs/recipes/scripts/0001-mvm-image-method2.py
@@ -3,7 +3,7 @@ from iiif_prezi3 import Manifest, Canvas, AnnotationPage, Annotation, Annotation
 config.configs['helpers.auto_fields.AutoLang'].auto_lang = "en"
 base_url = "https://iiif.io/api/cookbook/recipe/0001-mvm-image"
 
-manifest = Manifest(id=f"{base_url}/manifest.json", label="Single Image Example")
+manifest = Manifest(id=f"{base_url}/manifest.json", label="Simplest Image Example (IIIF Presentation v3)")
 canvas = Canvas(id=f"{base_url}/canvas/p1", height=1800, width=1200)
 anno_body = AnnotationBody(id="https://iiif.io/api/presentation/2.1/example/fixtures/resources/page1-full.png",
                            type="Image",
